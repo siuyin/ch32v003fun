7bdc79c154a3d670066b71db104afd43dce4dc1a
Loh Siu Yin <siuyin@beyondbroadcast.com>
2023-05-29 18:23:47 +0800

    Updated PWM timer example.

--- examples/tim1_pwm/tim1_pwm.c
+++ examples/tim1_pwm/tim1_pwm.c
@@ -1,6 +1,7 @@
 /*
  * Example for using Advanced Control Timer (TIM1) for PWM generation
  * 03-28-2023 E. Brombaugh
+ * 05-29-2023 SiuYin Loh
  */
 
 // Could be defined here, or in the processor defines.
@@ -28,16 +29,19 @@ void t1pwm_init( void )
 	GPIOC->CFGLR |= (GPIO_Speed_10MHz | GPIO_CNF_OUT_PP_AF)<<(4*4);
 		
 	// Reset TIM1 to init all regs
-	RCC->APB2PRSTR |= RCC_APB2Periph_TIM1;
-	RCC->APB2PRSTR &= ~RCC_APB2Periph_TIM1;
+	RCC->APB2PRSTR |= RCC_APB2Periph_TIM1; // writing a 1 will hold TIM1 in reset
+	RCC->APB2PRSTR &= ~RCC_APB2Periph_TIM1;// release from reset
 	
 	// CTLR1: default is up, events generated, edge align
 	// SMCFGR: default clk input is CK_INT
 	
 	// Prescaler 
 	TIM1->PSC = 0x0000;
+	//TIM1->PSC = 0x3FFF;
 	
+	TIM1->CTLR1 |= TIM_ARPE; // auto reload preload enable
 	// Auto Reload - sets period
+	//TIM1->ATRLR = 255;
 	TIM1->ATRLR = 255;
 	
 	// Reload immediately
@@ -45,15 +49,24 @@ void t1pwm_init( void )
 	
 	// Enable CH1N output, positive pol
 	TIM1->CCER |= TIM_CC1NE | TIM_CC1NP;
+	//TIM1->CCER |= TIM_CC1NE;
+	//TIM1->CCER &= ~TIM_CC1NP; // update to reset state. negative polarity
 	
 	// Enable CH4 output, positive pol
-	TIM1->CCER |= TIM_CC4E | TIM_CC4P;
+	//TIM1->CCER |= TIM_CC4E | TIM_CC4P;
+	TIM1->CCER |= TIM_CC4E; 
 	
 	// CH1 Mode is output, PWM1 (CC1S = 00, OC1M = 110)
-	TIM1->CHCTLR1 |= TIM_OC1M_2 | TIM_OC1M_1;
+	/* In PWM Mode 1, the timer ouput is not asserted when:
+	   In up-counter mode,
+	   counter is less than the compare value.
+	 */
+	TIM1->CHCTLR1 |= TIM_OC1M_2 | TIM_OC1M_1; // mode 1 PWM
+	//TIM1->CHCTLR1 |= TIM_OC1M_2 | TIM_OC1M_1 | TIM_OC1M_0; // mode 2 PWM
 	
-	// CH2 Mode is output, PWM1 (CC1S = 00, OC1M = 110)
-	TIM1->CHCTLR2 |= TIM_OC4M_2 | TIM_OC4M_1;
+	// CH4 Mode is output, PWM1 (CC1S = 00, OC1M = 110)
+	//TIM1->CHCTLR2 |= TIM_OC4M_2 | TIM_OC4M_1; // mode 1 PWM
+	TIM1->CHCTLR2 |= TIM_OC4M_2 | TIM_OC4M_1 | TIM_OC4M_0; // mode 2 PWM
 	
 	// Set the Capture Compare Register value to 50% initially
 	TIM1->CH1CVR = 128;
@@ -111,7 +124,7 @@ void t1pwm_force(uint8_t chl, uint8_t val)
  */
 int main()
 {
-	uint32_t count = 0;
+	uint8_t count = 0;
 	
 	SystemInit48HSI();
 
@@ -126,12 +139,14 @@ int main()
 	printf("done.\n\r");
 		
 	printf("looping...\n\r");
+	//const uint8_t pwmWidth[10]={0,1,2,5,10,20,50,100,200,255}; // approx Log10 scale
+	const uint8_t pwmWidth[10]={0,77,122,154,178,198,216,230,243,255}; // closer Log10 scale
 	while(1)
 	{
-		t1pwm_setpw(0, count); // Chl 1
-		t1pwm_setpw(3, (count + 128)&255);	// Chl 4
+		t1pwm_setpw(0, pwmWidth[count]); // Chl 1
+		t1pwm_setpw(3, pwmWidth[10-count]); // Chl 4
 		count++;
-		count &= 255;
-		Delay_Ms( 5 );
+		if (count >= 10) count=0;
+		Delay_Ms( 65 );
 	}
 }
