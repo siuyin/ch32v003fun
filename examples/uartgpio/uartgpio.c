f18dc195203ad42ed32843928c82aafd96fc7321
Loh Siu Yin <siuyin@beyondbroadcast.com>
2023-05-29 13:29:43 +0800

    Added button push input to uartgpio.c

--- examples/uartgpio/uartgpio.c
+++ examples/uartgpio/uartgpio.c
@@ -2,8 +2,9 @@
    to control GPIO.
   
    Connect an LED to PD0,
-   PD5 (TX) -> your terminal RX
-   PD6 (RX) -> your terminal TX
+   push button PD5 to Gnd,
+   PD5 (TX) -> your terminal RX,
+   PD6 (RX) -> your terminal TX.
   
    Communications settings: 115200 8 N 1
   
@@ -22,20 +23,25 @@ void initUART() {
 	USART1->CTLR1 |= USART_CTLR1_RE; // Rx enable
 }
 
-void initGPIOD0PushPull() {
+void initGPIOD0PushPullD4InputPullUp() {
 	// Enable GPIOD.
 	RCC->APB2PCENR |= RCC_APB2Periph_GPIOD;
 
 	// GPIO D0 Push-Pull, 10MHz Output
 	GPIOD->CFGLR &= ~(0xf<<(4*0));
 	GPIOD->CFGLR |= (GPIO_Speed_10MHz | GPIO_CNF_OUT_PP)<<(4*0);
+
+	// GPIO D4 Input Pull-up
+	GPIOD->CFGLR &= ~(0xf<<(4*4));
+	GPIOD->CFGLR |= GPIO_CNF_IN_PUPD<<(4*4);
+	GPIOD->OUTDR |= 1<<4; // PD4 pull-up
 }
 
 int main()
 {
 	SystemInit48HSI();
 	initUART();
-	initGPIOD0PushPull();
+	initGPIOD0PushPullD4InputPullUp();
 
 
 	printf("UART is now controlling GPIO. Type h for help.\n");
@@ -43,6 +49,7 @@ int main()
 	#define CHAR_RECVD ((USART1->STATR & USART_STATR_RXNE) != 0)
 	#define LED_ON (GPIOD->BSHR = 1)
 	#define LED_OFF (GPIOD->BSHR = 1<<16)
+	#define BTN_PUSHED ((GPIOD->INDR&(1<<4)) == 0)
 	while(1)
 	{
 		if (CHAR_RECVD) {
@@ -60,12 +67,16 @@ int main()
 				break;
 			case '?':
 			case 'h':
-				printf("available commands: 1 = LED on, 0 = LED off\n");
+				printf("available commands: 1 = LED on, 0 = LED off\nPressing the push-button sends \"Button Pushed\" message.\n");
 				break;
 			default:
 				printf("unknown command\n");
 				break;
 			}
+		}
+
+		if (BTN_PUSHED) {
+			printf("Button Pushed\n");
 		}
 	}
 }
