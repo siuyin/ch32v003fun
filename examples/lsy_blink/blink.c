916a9ca9cab50a4cbad6dc4163737dd7e39517c0
Loh Siu Yin <siuyin@beyondbroadcast.com>
2025-01-15 16:52:02 +0800

    feat: add my reduced version of blink

---
+++ examples/lsy_blink/blink.c
@@ -0,0 +1,23 @@
+#include "ch32v003fun.h"
+#include <stdio.h>
+
+// use defines to make more meaningful names for our GPIO pins
+#define PIN_1 PD0
+
+int main()
+{
+	SystemInit();
+
+	// Enable GPIOs
+	funGpioInitAll();
+	
+	funPinMode( PIN_1,     GPIO_Speed_10MHz | GPIO_CNF_OUT_PP );
+
+	while(1)
+	{
+		funDigitalWrite( PIN_1,     FUN_HIGH );
+		Delay_Ms( 250 );
+		funDigitalWrite( PIN_1,     FUN_LOW );
+		Delay_Ms( 250 );
+	}
+}
