dac42b27ab6069563843fe93808312ef5cc9acbf
Loh Siu Yin <siuyin@beyondbroadcast.com>
2025-01-21 18:04:29 +0800

    wip: statemachine

--- examples/statemachine/statemachine.c
+++ examples/statemachine/statemachine.c
@@ -48,6 +48,84 @@ void toggle_button(){
 	btn_next_check_tick = SysTick->CNT + btn_check_interval;
 }
 
+void blink_led(){
+	funDigitalWrite(LED,FUN_HIGH);
+	Delay_Ms(50);
+	funDigitalWrite(LED,FUN_LOW);
+}
+
+void double_blink_led(){
+	funDigitalWrite(LED,FUN_HIGH);
+	Delay_Ms(50);
+	funDigitalWrite(LED,FUN_LOW);
+	Delay_Ms(150);
+	funDigitalWrite(LED,FUN_HIGH);
+	Delay_Ms(50);
+	funDigitalWrite(LED,FUN_LOW);
+	Delay_Ms(50);
+}
+
+typedef enum {
+	BtnDblClickStateReleased,
+	BtnDblClickStateCandPushed,
+	BtnDblClickStatePushed,
+	BtnDblClickStateCandLongClicked,
+	BtnDblClickStateLongClicked,
+	BtnDblClickStateCandReleasedForSingleOrDoubleClick,
+	BtnDblClickStateReleasedForSingleOrDoubleClick,
+	BtnDblClickStateSingleClicked,
+	BtnDblClickStateDoubleClicked,
+	BtnDblClickStateCandReleased,
+} BtnDblClkState_Typedef;
+
+uint32_t long_click_max_interval = Ticks_from_Ms(750);
+uint32_t double_click_max_interval = Ticks_from_Ms(350); // tune this value to properly detect a double-click
+volatile uint32_t interval_timer_start_tick;
+BtnDblClkState_Typedef dblclick_state = BtnDblClickStateReleased;
+// statemachine markdown:
+// [![](https://mermaid.ink/img/pako:eNqVlE1P4zAQhv_KyMdVGrVuUqfRCg4gTnys4LTQPZh4mkQ4duU4CLbqf8dxICTQLmxO8fiZ1-94NN6STAskKaktt3ha8tzwavJIVwrcd_fjD0wmR3CNEnmNoosqbREkri3odb-VworcN9ZqBZumLiArMHtAAaUC5FkBXn9FOoW3JC9-wpUIf7kct07hd0cMg0MHjrjcT3S_TiBozzIWbFmh6dgB5tPOtcpPZNka7E8cVfWZGtTmlNx2pw9HIB0GWcuBLQzWhZbiGDAPgcXTqh64HQh6L_tsfGR84vslX_5DboSeaXNTqlzilTnVzb1ETzqBAJzHjxf0jdRRGw4gvb9vSA3Wh7rwVS2HevIThMf2dQXm074pIwufr_C_yunifTmviaPoV_0cTcUAIQFxVVW8FG5Ot23CitgCKzdO7dQJXPNG2na2dg7ljdU3zyojqTUNBsToJi9IuuaydqtmI97n_A3ZcHWrdb9EUVptLrp3wT8PHiHpljyRlLIwmUUsShbLKaURjQPyTNLJPIlDxpZJvFjM2GLKZvEuIH-9Kg0ZXUYJjWZ0Pk9ozHYvDyJztw?type=png)](https://mermaid.live/edit#pako:eNqVlE1P4zAQhv_KyMdVGrVuUqfRCg4gTnys4LTQPZh4mkQ4duU4CLbqf8dxICTQLmxO8fiZ1-94NN6STAskKaktt3ha8tzwavJIVwrcd_fjD0wmR3CNEnmNoosqbREkri3odb-VworcN9ZqBZumLiArMHtAAaUC5FkBXn9FOoW3JC9-wpUIf7kct07hd0cMg0MHjrjcT3S_TiBozzIWbFmh6dgB5tPOtcpPZNka7E8cVfWZGtTmlNx2pw9HIB0GWcuBLQzWhZbiGDAPgcXTqh64HQh6L_tsfGR84vslX_5DboSeaXNTqlzilTnVzb1ETzqBAJzHjxf0jdRRGw4gvb9vSA3Wh7rwVS2HevIThMf2dQXm074pIwufr_C_yunifTmviaPoV_0cTcUAIQFxVVW8FG5Ot23CitgCKzdO7dQJXPNG2na2dg7ljdU3zyojqTUNBsToJi9IuuaydqtmI97n_A3ZcHWrdb9EUVptLrp3wT8PHiHpljyRlLIwmUUsShbLKaURjQPyTNLJPIlDxpZJvFjM2GLKZvEuIH-9Kg0ZXUYJjWZ0Pk9ozHYvDyJztw)
+void indicate_led_on_single_and_double_click(){
+	if (btn_next_check_tick>SysTick->CNT) {
+		return;
+	}
+
+	printf("dblclick state=%d\n",dblclick_state);
+	switch (dblclick_state) {
+		case BtnDblClickStateReleased:
+			if (funDigitalRead(BTN) == 0) {
+				dblclick_state = BtnDblClickStateCandPushed;
+			}
+			break;
+		case BtnDblClickStateCandPushed:
+			if (funDigitalRead(BTN) == 0) {
+				dblclick_state = BtnDblClickStatePushed;
+				interval_timer_start_tick = SysTick->CNT;
+			}
+			break;
+		case BtnDblClickStatePushed:
+			if (funDigitalRead(BTN) == 0 && (SysTick->CNT - interval_timer_start_tick > long_click_max_interval)) {
+				dblclick_state = BtnDblClickStateLongClicked;
+				double_blink_led();
+				break;
+			}
+			dblclick_state = BtnDblClickStateCandReleased;
+			blink_led();
+			break;
+		case BtnDblClickStateCandLongClicked:
+			if (funDigitalRead(BTN) == 1) {
+				dblclick_state = BtnDblClickStateCandReleased;
+			}
+			break;
+		case BtnDblClickStateCandReleased:
+			if (funDigitalRead(BTN) == 1) {
+				dblclick_state = BtnDblClickStateReleased;
+			}
+			break;
+	}
+	btn_next_check_tick = SysTick->CNT + btn_check_interval;
+}
+
+
 int main()
 {
 	SystemInit();
@@ -63,6 +141,7 @@ int main()
 	printf("Entering main loop\n");
 	btn_next_check_tick = SysTick->CNT + btn_check_interval;
 	while(1) {
-		toggle_button();
+		//toggle_button();
+		indicate_led_on_single_and_double_click();
 	}
 }
