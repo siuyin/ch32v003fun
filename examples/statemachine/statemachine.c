8a3116af84286b0dd8e9c962b87482f5f0a7eba2
Loh Siu Yin <siuyin@beyondbroadcast.com>
2025-01-23 10:04:52 +0800

    wip: statemachine

--- examples/statemachine/statemachine.c
+++ examples/statemachine/statemachine.c
@@ -1,8 +1,16 @@
 #include "ch32v003fun.h"
 #include <stdio.h>
+#include <stdbool.h>
 
 #define BTN PD2
 #define LED PD0
+
+// bool btn_pushed() {
+// 	if (funDigitalRead(BTN) == 0) return true;
+// 	return false;
+// }
+
+#define btn_pushed() ( funDigitalRead(BTN) == 0 ? 1 : 0 )
 
 typedef enum {
 	BtnStateReleased,
@@ -66,24 +74,24 @@ void double_blink_led(){
 }
 
 typedef enum {
-	BtnDblClickStateReleased,
-	BtnDblClickStateCandPushed,
-	BtnDblClickStatePushed,
-	BtnDblClickStateCandLongClicked,
-	BtnDblClickStateLongClicked,
-	BtnDblClickStateCandReleasedForSingleOrDoubleClick,
-	BtnDblClickStateReleasedForSingleOrDoubleClick,
-	BtnDblClickStateSingleClicked,
-	BtnDblClickStateDoubleClicked,
-	BtnDblClickStateCandReleased,
+	BDCSReleased,
+	BDCSCandPushed,
+	BDCSPushed,
+	BDCSCandLongClicked,
+	BDCSLongClicked,
+	BDCSCandReleasedForSingleOrDoubleClick,
+	BDCSReleasedForSingleOrDoubleClick,
+	BDCSSingleClicked,
+	BDCSDoubleClicked,
+	BDCSCandReleased,
 } BtnDblClkState_Typedef;
 
-uint32_t long_click_max_interval = Ticks_from_Ms(750);
-uint32_t double_click_max_interval = Ticks_from_Ms(350); // tune this value to properly detect a double-click
+uint32_t long_click_max_interval = Ticks_from_Ms(500);
+uint32_t double_click_max_interval = Ticks_from_Ms(250); // tune this value to properly detect a double-click
 volatile uint32_t interval_timer_start_tick;
-BtnDblClkState_Typedef dblclick_state = BtnDblClickStateReleased;
+BtnDblClkState_Typedef dblclick_state = BDCSReleased;
 // statemachine markdown:
-// [![](https://mermaid.ink/img/pako:eNqVlE1P4zAQhv_KyMdVGrVuUqfRCg4gTnys4LTQPZh4mkQ4duU4CLbqf8dxICTQLmxO8fiZ1-94NN6STAskKaktt3ha8tzwavJIVwrcd_fjD0wmR3CNEnmNoosqbREkri3odb-VworcN9ZqBZumLiArMHtAAaUC5FkBXn9FOoW3JC9-wpUIf7kct07hd0cMg0MHjrjcT3S_TiBozzIWbFmh6dgB5tPOtcpPZNka7E8cVfWZGtTmlNx2pw9HIB0GWcuBLQzWhZbiGDAPgcXTqh64HQh6L_tsfGR84vslX_5DboSeaXNTqlzilTnVzb1ETzqBAJzHjxf0jdRRGw4gvb9vSA3Wh7rwVS2HevIThMf2dQXm074pIwufr_C_yunifTmviaPoV_0cTcUAIQFxVVW8FG5Ot23CitgCKzdO7dQJXPNG2na2dg7ljdU3zyojqTUNBsToJi9IuuaydqtmI97n_A3ZcHWrdb9EUVptLrp3wT8PHiHpljyRlLIwmUUsShbLKaURjQPyTNLJPIlDxpZJvFjM2GLKZvEuIH-9Kg0ZXUYJjWZ0Pk9ozHYvDyJztw?type=png)](https://mermaid.live/edit#pako:eNqVlE1P4zAQhv_KyMdVGrVuUqfRCg4gTnys4LTQPZh4mkQ4duU4CLbqf8dxICTQLmxO8fiZ1-94NN6STAskKaktt3ha8tzwavJIVwrcd_fjD0wmR3CNEnmNoosqbREkri3odb-VworcN9ZqBZumLiArMHtAAaUC5FkBXn9FOoW3JC9-wpUIf7kct07hd0cMg0MHjrjcT3S_TiBozzIWbFmh6dgB5tPOtcpPZNka7E8cVfWZGtTmlNx2pw9HIB0GWcuBLQzWhZbiGDAPgcXTqh64HQh6L_tsfGR84vslX_5DboSeaXNTqlzilTnVzb1ETzqBAJzHjxf0jdRRGw4gvb9vSA3Wh7rwVS2HevIThMf2dQXm074pIwufr_C_yunifTmviaPoV_0cTcUAIQFxVVW8FG5Ot23CitgCKzdO7dQJXPNG2na2dg7ljdU3zyojqTUNBsToJi9IuuaydqtmI97n_A3ZcHWrdb9EUVptLrp3wT8PHiHpljyRlLIwmUUsShbLKaURjQPyTNLJPIlDxpZJvFjM2GLKZvEuIH-9Kg0ZXUYJjWZ0Pk9ozHYvDyJztw)
+// [![](https://mermaid.ink/img/pako:eNqVVEtvgkAQ_iubOTZAQUAeab1oemvStLeKh5VdhZSHWZam1vjfuyziAxQpJ77he8xMlt1BmBMKPhQcczqL8ZrhVP0eBRkSz_xhgVR1gt5pQnFBSV1tkPw0xRnR3soiEthH8yXP0EaixRXyERyY7IAP3HOvit5j26ZKPMT-X813FXcihjXf7vslZgWfJnH41d_7Ba9X3Z2io-0NRY9IHAjGEY9Tymqb6-mzZRJWtcpCktHTMyJNkUeMFtrilsEpvZZObihviLqTtrsbciKOE1SqBvSt8kLQWcKVmDPTARktdv8coIBYXIpjIv7hXWUQAI9oSgPwxSuhK1wmPIAg2wsqLnn-sc1C8DkrqQIsL9cR-CucFAKVG3K6AxrKBmefeX6ElMQ8Z6_1nSGvDkkBfwc_4KuuZ2m2Y1i2Z-uu61qOAtuqbJiaaemG4RjjkeGZo70Cv9LV1HTTHnu6ZTuOa5tjz9r_AbCVdv0?type=png)](https://mermaid.live/edit#pako:eNqVVEtvgkAQ_iubOTZAQUAeab1oemvStLeKh5VdhZSHWZam1vjfuyziAxQpJ77he8xMlt1BmBMKPhQcczqL8ZrhVP0eBRkSz_xhgVR1gt5pQnFBSV1tkPw0xRnR3soiEthH8yXP0EaixRXyERyY7IAP3HOvit5j26ZKPMT-X813FXcihjXf7vslZgWfJnH41d_7Ba9X3Z2io-0NRY9IHAjGEY9Tymqb6-mzZRJWtcpCktHTMyJNkUeMFtrilsEpvZZObihviLqTtrsbciKOE1SqBvSt8kLQWcKVmDPTARktdv8coIBYXIpjIv7hXWUQAI9oSgPwxSuhK1wmPIAg2wsqLnn-sc1C8DkrqQIsL9cR-CucFAKVG3K6AxrKBmefeX6ElMQ8Z6_1nSGvDkkBfwc_4KuuZ2m2Y1i2Z-uu61qOAtuqbJiaaemG4RjjkeGZo70Cv9LV1HTTHnu6ZTuOa5tjz9r_AbCVdv0)
 void indicate_led_on_single_and_double_click(){
 	if (btn_next_check_tick>SysTick->CNT) {
 		return;
@@ -91,27 +99,27 @@ void indicate_led_on_single_and_double_click(){
 
 	printf("dblclick state=%d\n",dblclick_state);
 	switch (dblclick_state) {
-		case BtnDblClickStateReleased:
+		case BDCSReleased:
 			if (funDigitalRead(BTN) == 0) {
-				dblclick_state = BtnDblClickStateCandPushed;
+				dblclick_state = BDCSCandPushed;
 			}
 			break;
-		case BtnDblClickStateCandPushed:
+		case BDCSCandPushed:
 			if (funDigitalRead(BTN) == 0) {
-				dblclick_state = BtnDblClickStatePushed;
+				dblclick_state = BDCSPushed;
 				interval_timer_start_tick = SysTick->CNT;
 			}
 
 			break;
-		case BtnDblClickStatePushed:
+		case BDCSPushed:
 			if (funDigitalRead(BTN) == 0) {
-				dblclick_state = BtnDblClickStateCandLongClicked;
+				dblclick_state = BDCSCandLongClicked;
 			}
 			break;
-		case BtnDblClickStateCandLongClicked:
+		case BDCSCandLongClicked:
 			if (funDigitalRead(BTN) == 0){
 				if (SysTick->CNT - interval_timer_start_tick > long_click_max_interval) {
-					dblclick_state = BtnDblClickStateLongClicked;
+					dblclick_state = BDCSLongClicked;
 					double_blink_led();
 					blink_led();
 					break;
@@ -119,22 +127,22 @@ void indicate_led_on_single_and_double_click(){
 				break;
 			}
 			interval_timer_start_tick = SysTick->CNT;
-			dblclick_state = BtnDblClickStateCandReleasedForSingleOrDoubleClick;
+			dblclick_state = BDCSCandReleasedForSingleOrDoubleClick;
 			break;
-		case BtnDblClickStateLongClicked:
+		case BDCSLongClicked:
 			if (funDigitalRead(BTN) == 1) {
-				dblclick_state = BtnDblClickStateCandReleased;
+				dblclick_state = BDCSCandReleased;
 			}
 			break;
-		case BtnDblClickStateCandReleasedForSingleOrDoubleClick:
+		case BDCSCandReleasedForSingleOrDoubleClick:
 			if (funDigitalRead(BTN) == 1) {
-				dblclick_state = BtnDblClickStateReleasedForSingleOrDoubleClick;
+				dblclick_state = BDCSReleasedForSingleOrDoubleClick;
 			}
 			break;
-		case BtnDblClickStateReleasedForSingleOrDoubleClick:
+		case BDCSReleasedForSingleOrDoubleClick:
 			while (SysTick->CNT - interval_timer_start_tick < double_click_max_interval) {
 				if (funDigitalRead(BTN) == 0) {
-					dblclick_state = BtnDblClickStateDoubleClicked;
+					dblclick_state = BDCSDoubleClicked;
 					double_blink_led();
 					goto end;
 				}
@@ -143,32 +151,113 @@ void indicate_led_on_single_and_double_click(){
 			if (funDigitalRead(BTN) == 0) {
 				if (SysTick->CNT - interval_timer_start_tick > double_click_max_interval) {
 				}
-				dblclick_state = BtnDblClickStateSingleClicked;
+				dblclick_state = BDCSSingleClicked;
 				blink_led();
 				break;
 			}
-			dblclick_state = BtnDblClickStateCandReleased;
+			dblclick_state = BDCSCandReleased;
 end:
 			break;
-		case BtnDblClickStateSingleClicked:
+		case BDCSSingleClicked:
 			if (funDigitalRead(BTN) == 1) {
-				dblclick_state = BtnDblClickStateCandReleased;
+				dblclick_state = BDCSCandReleased;
 			}
 			break;
-		case BtnDblClickStateDoubleClicked:
+		case BDCSDoubleClicked:
 			if (funDigitalRead(BTN) == 1) {
-				dblclick_state = BtnDblClickStateCandReleased;
+				dblclick_state = BDCSCandReleased;
 			}
 			break;
-		case BtnDblClickStateCandReleased:
+		case BDCSCandReleased:
 			if (funDigitalRead(BTN) == 1) {
-				dblclick_state = BtnDblClickStateReleased;
+				dblclick_state = BDCSReleased;
 			}
 			break;
 	}
 	btn_next_check_tick = SysTick->CNT + btn_check_interval;
 }
 
+typedef enum {
+	LCSReleased,
+	LCSCandPushed,
+	LCSPushed,
+	LCSCandLongClicked,
+	LCSLongClicked,
+	LCSClicked,
+	LCSCandReleased
+} LongClickState;
+
+LongClickState lc_state = LCSReleased;
+// state diagram markdown:
+// [![](https://mermaid.ink/img/pako:eNqdk09TgzAQxb9KZo8OIElopDn0Uo864-jN0kOEtDBC0gnBsXb63U3pH1uk0yonXvjt27dkdgWpziRwqK2w8r4QcyMq_4MkCrlncjNFvj9Cz7KUopbZ9nSv2k9jobLgqalzpzmavFmFFq2a9sAHsSPNTu_YY68N3muLbpHLaiyyRSVNf2Grr2n253S_Z-9WnUzfzfSg1XxcFun7Ffwp1hfm2O2_Jdek6q08W4Qcvb0cNEKlo1C6wZDNjaxzXR5czyc_7d6Ne-GeOkEvDdf7R862AA_cYJUoMrczq41DAjaXlUyAu9dMzkRT2gQStXaoaKx-WaoUuDWN9MDoZp4Dn4mydqpZZD87t0cWQr1qfZAyK6w2j9sdbVe1RYCv4BO4j4csYCGOKI4xCQfEg6U7JSQO7gY4ZIxQEjMakbUHX60rDiJCGYtDwoYkwpTS9TdmL0hz?type=png)](https://mermaid.live/edit#pako:eNqdk09TgzAQxb9KZo8OIElopDn0Uo864-jN0kOEtDBC0gnBsXb63U3pH1uk0yonXvjt27dkdgWpziRwqK2w8r4QcyMq_4MkCrlncjNFvj9Cz7KUopbZ9nSv2k9jobLgqalzpzmavFmFFq2a9sAHsSPNTu_YY68N3muLbpHLaiyyRSVNf2Grr2n253S_Z-9WnUzfzfSg1XxcFun7Ffwp1hfm2O2_Jdek6q08W4Qcvb0cNEKlo1C6wZDNjaxzXR5czyc_7d6Ne-GeOkEvDdf7R862AA_cYJUoMrczq41DAjaXlUyAu9dMzkRT2gQStXaoaKx-WaoUuDWN9MDoZp4Dn4mydqpZZD87t0cWQr1qfZAyK6w2j9sdbVe1RYCv4BO4j4csYCGOKI4xCQfEg6U7JSQO7gY4ZIxQEjMakbUHX60rDiJCGYtDwoYkwpTS9TdmL0hz)
+void flash_led_on_single_and_long_click() {
+	if (btn_next_check_tick>SysTick->CNT) {
+		return;
+	}
+
+	printf("state=%d pushed=%d\n",lc_state,btn_pushed());
+	switch(lc_state) {
+		case LCSReleased:
+			if (btn_pushed()) {
+				lc_state = LCSCandPushed;
+				break;
+			}
+			lc_state = LCSReleased;
+			break;
+		case LCSCandPushed:
+			if (btn_pushed()) {
+				lc_state = LCSPushed;
+				interval_timer_start_tick = SysTick->CNT;
+				break;
+			}
+			lc_state = LCSCandReleased;
+			break;
+		case LCSPushed:
+			if (btn_pushed()) {
+				lc_state = LCSCandLongClicked;
+				break;
+			}
+			lc_state = LCSClicked;
+			blink_led();
+			break;
+		case LCSCandLongClicked:
+			if (btn_pushed()) {
+				if (SysTick->CNT - interval_timer_start_tick > long_click_max_interval) {
+					lc_state = LCSLongClicked;
+					double_blink_led();
+					break;
+				}
+				lc_state = LCSCandLongClicked;
+				break;
+			}
+			lc_state = LCSClicked;
+			blink_led();
+			break;
+		case LCSLongClicked:
+			if (btn_pushed()) {
+				lc_state = LCSLongClicked;
+				break;
+			}
+			lc_state = LCSCandReleased;
+			break;
+		case LCSClicked:
+			if (btn_pushed()) {
+				lc_state = LCSClicked;
+				break;
+			}
+			lc_state = LCSCandReleased;
+			break;
+		case LCSCandReleased:
+			if (btn_pushed()) {
+				lc_state = LCSCandReleased;
+				break;
+			}
+			lc_state = LCSReleased;
+			break;
+	}
+
+	btn_next_check_tick = SysTick->CNT + btn_check_interval;
+}
 
 int main() {
 	SystemInit();
@@ -185,6 +274,7 @@ int main() {
 	btn_next_check_tick = SysTick->CNT + btn_check_interval;
 	while(1) {
 		//toggle_button();
-		indicate_led_on_single_and_double_click();
+		//indicate_led_on_single_and_double_click();
+		flash_led_on_single_and_long_click();
 	}
 }
